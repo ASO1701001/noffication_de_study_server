d39eb57a367941dad63a81f976eed5f1bcd72127
ASO1701001 <1701001@st.asojuku.ac.jp>
2019-07-31 11:34:47 +0900

    Last Commit.

--- api/add-answer.php
+++ api/add-answer.php
@@ -4,12 +4,8 @@ require_once '../libs/RequestManager.php';
 if (!isset($_POST['user_id']) || !isset($_POST['question_id']) || !isset($_POST['answer_choice']) || !isset($_POST['answer_time'])) {
     $json = ['status' => 'E00', 'msg' => 'REQUIRED_PARAM'];
 } else {
-    $data = RequestManager::addAnswer($_POST['user_id'], $_POST['question_id'], $_POST['answer_choice'], $_POST['answer_time']);
-    if (!$data) {
-        $json = ['status' => 'E00', 'msg' => 'FOREIGN_KEY_ERROR'];
-    } else {
-        $json = ['status' => 'S00', 'data' => ['user_id' => $data]];
-    }
+    RequestManager::addAnswer($_POST['user_id'], $_POST['question_id'], $_POST['answer_choice'], $_POST['answer_time']);
+    $json = ['status' => 'S00'];
 }
 
 header('Access-Control-Allow-Origin: *');
--- libs/RequestManager.php
+++ libs/RequestManager.php
@@ -57,9 +57,11 @@ class RequestManager {
         $data['questions_genres_db'] = $db->fetchAll($sql, $array);
 
         // correct_answer_db
+        /*
         $sql = "SELECT * FROM correct_answer_db";
         $array = array();
         $data['correct_answer_db'] = $db->fetchAll($sql, $array);
+        */
 
         return $data;
     }
@@ -73,17 +75,24 @@ class RequestManager {
         $db = new DatabaseManager();
         require_once 'functions.php';
 
-        do {
-            $user_id = random(8);
-            $count = $db->fetch("SELECT count(*) FROM users WHERE user_id = :user_id", array('user_id' => $user_id));
-            if ($count != 0) {
-                break;
-            }
-        } while (true);
+        $sql = "SELECT count(*) FROM users WHERE token = :token";
+        $count = $db->fetchColumn($sql, array('token' => $token));
+        if ($count == 0) {
+            do {
+                $user_id = random(8);
+                $count = $db->fetch("SELECT count(*) FROM users WHERE user_id = :user_id", array('user_id' => $user_id));
+                if ($count != 0) {
+                    break;
+                }
+            } while (true);
 
-        $sql = "INSERT INTO users(user_id, token) VALUES (:user_id, :token)";
-        $array = array('user_id' => $user_id, 'token' => $token);
-        $db->insert($sql, $array);
+            $sql = "INSERT INTO users(user_id, token) VALUES (:user_id, :token)";
+            $array = array('user_id' => $user_id, 'token' => $token);
+            $db->insert($sql, $array);
+        } else {
+            $sql = "SELECT user_id FROM users WHERE token = :token";
+            $user_id = $db->fetchColumn($sql, array('token' => $token));
+        }
 
         return $user_id;
     }
@@ -104,17 +113,37 @@ class RequestManager {
      * @param $question_id
      * @param $answer_choice
      * @param $answer_time
-     * @return string
      */
     public static function addAnswer($user_id, $question_id, $answer_choice, $answer_time) {
         require_once 'DatabaseManager.php';
         $db = new DatabaseManager();
         $sql = "INSERT INTO all_user_answers(user_id, question_id, answer_choice, answer_time) VALUES (:user_id, :question_id, :answer_choice, :answer_time)";
         $array = array('user_id' => $user_id, 'question_id' => $question_id, 'answer_choice' => $answer_choice, 'answer_time' => $answer_time);
-        $id = $db->insert($sql, $array);
-        if (!$id) {
-            return false;
-        }
-        return $id;
+        $db->insert($sql, $array);
+    }
+
+    /* 回答レート更新
+     *
+     */
+    public static function updateRate() {
+        require_once 'DatabaseManager.php';
+        $db = new DatabaseManager();
+        $sql = "UPDATE answers_rate_db, (
+                    SELECT a.question_id, ROUND((totalCollectCount / totalAnswerCount), 3) answer_rate
+                    FROM
+                        (SELECT question_id, count(*) totalAnswerCount
+                        FROM all_user_answers
+                        GROUP BY question_id) as a,
+                
+                        (SELECT aua.question_id, count(*) totalCollectCount
+                        FROM all_user_answers aua, answer_db ad
+                        WHERE aua.question_id = ad.question_id
+                        AND aua.answer_choice = ad.answer_number
+                        GROUP BY question_id) as b
+                    WHERE a.question_id = b.question_id
+                    ) as tmp
+                SET answers_rate_db.answer_rate = tmp.answer_rate
+                WHERE answers_rate_db.question_id = tmp.question_id";
+        $db->execute($sql, array());
     }
 }
